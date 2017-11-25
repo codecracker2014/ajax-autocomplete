0268569b1aa53abee75165f44cff57b31711020a
codecracker2014 <raaz2.gajendra@gmail.com>
2017-11-26 00:42:26 +0530

    Build setup

--- src/ajax-autocomplete/js/test.js
+++ src/ajax-autocomplete/js/test.js
@@ -1,4 +1,24 @@
-function test()
-{
-    console.log("HI");
-}
+(function($) {
+
+        var chosenAutocomplete=function(options)
+        {
+            console.log(options);
+        }
+
+        $.fn.chosenAutocomplete = function( options ) {
+    
+            // Establish our default settings
+            var settings = $.extend({
+                text         : 'Hello, World!',
+                color        : null,
+                fontStyle    : null
+            }, options);
+    
+            return this.each( function() {
+                $(this).data("chosenAutocomplete",new chosenAutocomplete(settings));
+            });    
+
+
+        }
+    
+    }(jQuery));
